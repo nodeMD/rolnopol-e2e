6595e8873e53e6828d8464fcccab9f4d37be7f4d
Maciej Dynieski <53050193+nodeMD@users.noreply.github.com>
2026-04-13 20:00:42 +0200

    config: set workers to 2 to prevent 429 responses

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 2 : 3,
+  workers: process.env.CI ? 2 : 2,
   reporter: [
     ['html', { open: 'never' }],
     ['junit', { outputFile: 'test-results/junit.xml' }],
